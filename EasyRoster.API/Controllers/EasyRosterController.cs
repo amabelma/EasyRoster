f6d7b685a6b4b3cde1febbb108bd85fc9b4daa2c
Mark Amabeli <11965165+amabelma@users.noreply.github.com>
2020-08-04 20:03:09 -0400

    Set up first method, still need to correctly read data from the post body.

---
+++ EasyRoster.API/Controllers/EasyRosterController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using EasyRoster.API.Repositories.MockRepositories;
+using EasyRoster.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.IO;
+
+namespace EasyRoster.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EasyRosterController : ControllerBase
+    {
+        private readonly ILogger<EasyRosterController> _logger;
+
+        public EasyRosterController(ILogger<EasyRosterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Player>> GetPlayersByRosterId(Guid rosterId)
+        {
+            using var reader = new StreamReader(Request.Body);
+            var body = reader.ReadToEndAsync();
+            var fml = body.Result;
+            _ = new List<Player>();
+            var mockPlayerRepository = new MockPlayerRepository();
+            var myTask = Task.Run(() => mockPlayerRepository.GetPlayersByRosterId(rosterId));
+            List<Player> playerList = await myTask;
+            return playerList;
+
+        }
+    }
+}

5c104dc5a7625a42f31b78e2ce4270f84ad451ca
mattamabeli <mattamabeli@gmail.com>
2020-08-22 22:58:30 -0400

    Started Implementing a generic Repository pattern. Researching Domain Driven design

--- EasyRoster.API/Repositories/PlayerRepository.cs
+++ EasyRoster.API/Repositories/PlayerRepository.cs
@@ -5,36 +5,7 @@ using System.Collections.Generic;
 
 namespace EasyRoster.API.Repositories
 {
-    class PlayerRepository : IPlayerRepository
-    {
-        public void DeletePlayer(Guid playerId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public Player GetPlayerById(Guid playerId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public List<Player> GetPlayersByRosterId(Guid rosterId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public List<Player> GetPlayersByTeamId(Guid teamId)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void InsertPlayer(Player player)
-        {
-            throw new System.NotImplementedException();
-        }
-
-        public void UpdatePlayer(Player player)
-        {
-            throw new System.NotImplementedException();
-        }
+    public class PlayerRepository : BaseRepository<Player>
+    { 
     }
 }

595b491f9020b6c2dccc4880d4593c32810b2e7d
Mark Amabeli <mark.amabeli@rhsps.com@users.noreply.github.com>
2020-09-26 15:07:14 -0400

    Fix typo in method name, comment out test for now due to pushing back finishing the tests due to a lack of understanding how contexts work.

--- ReziRoster.API.Tests/Repositories/BaseRepository.Tests.cs
+++ ReziRoster.API.Tests/Repositories/BaseRepository.Tests.cs
@@ -16,30 +16,30 @@ namespace ReziRoster.API.Repositories.Tests
     [TestFixture]
     public class GuardTest
     {
-        [Test]
-        public void BaseRepository_GetById_CallsDbSetWithPrividedId()
-        {
-            var mockUsers = new List<User>()
-            {
-                new User("Email", "FirstName", "LastName", "Password", "UserName"),
-                new User("Email2", "FirstName2", "LastName2", "Password2", "UserName2"),
-                new User("Email3", "FirstName3", "LastName3", "Password3", "UserName3"),
-            }.AsQueryable();
+        //[Test]
+        //public void BaseRepository_GetById_CallsDbSetWithProvidedId()
+        //{
+        //    var mockUsers = new List<User>()
+        //    {
+        //        new User("Email", "FirstName", "LastName", "Password", "UserName"),
+        //        new User("Email2", "FirstName2", "LastName2", "Password2", "UserName2"),
+        //        new User("Email3", "FirstName3", "LastName3", "Password3", "UserName3"),
+        //    }.AsQueryable();
 
-            var fakeDbSet = A.Fake<DbSet<User>>();
-            var fakeDbContext = A.Fake<UserContext>();
-            A.CallTo(() => fakeDbContext.User).Returns(fakeDbSet);
+        //    var fakeDbSet = A.Fake<DbSet<User>>();
+        //    var fakeDbContext = A.Fake<UserContext>();
+        //    A.CallTo(() => fakeDbContext.User).Returns(fakeDbSet);
 
 
-            A.CallTo(() => ((IQueryable<User>)fakeDbSet).Provider).Returns(mockUsers.Provider);
-            A.CallTo(() => ((IQueryable<User>)fakeDbSet).Expression).Returns(mockUsers.Expression);
-            A.CallTo(() => ((IQueryable<User>)fakeDbSet).ElementType).Returns(mockUsers.ElementType);
-            A.CallTo(() => ((IQueryable<User>)fakeDbSet).GetEnumerator()).Returns(mockUsers.GetEnumerator());
+        //    A.CallTo(() => ((IQueryable<User>)fakeDbSet).Provider).Returns(mockUsers.Provider);
+        //    A.CallTo(() => ((IQueryable<User>)fakeDbSet).Expression).Returns(mockUsers.Expression);
+        //    A.CallTo(() => ((IQueryable<User>)fakeDbSet).ElementType).Returns(mockUsers.ElementType);
+        //    A.CallTo(() => ((IQueryable<User>)fakeDbSet).GetEnumerator()).Returns(mockUsers.GetEnumerator());
 
-            var baseRepositoryUnderTest = new BaseRepository<User>(fakeDbContext);
-            var results = baseRepositoryUnderTest.GetByID(1);
+        //    var baseRepositoryUnderTest = new BaseRepository<User>(fakeDbContext);
+        //    var results = baseRepositoryUnderTest.GetByID(1);
 
             
-        }
+        //}
     }
 }
